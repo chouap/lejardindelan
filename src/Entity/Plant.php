abe5155a3259dea1ad9387fbb3026f783a399e46
Bao Nguyen <nguyen.bao.94@gmail.com>
2026-04-21 18:12:13 +0200

    Add plant entity

---
+++ src/Entity/Plant.php
@@ -0,0 +1,156 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlantRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PlantRepository::class)]
+class Plant
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $commonName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $scientificName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $shortDescription = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $longDescription = null;
+
+    #[ORM\Column(type: Types::DECIMAL, precision: 10, scale: 2)]
+    private ?string $price = null;
+
+    #[ORM\Column]
+    private ?bool $isAvailable = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $availableQuantity = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $mainImage = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommonName(): ?string
+    {
+        return $this->commonName;
+    }
+
+    public function setCommonName(string $commonName): static
+    {
+        $this->commonName = $commonName;
+
+        return $this;
+    }
+
+    public function getScientificName(): ?string
+    {
+        return $this->scientificName;
+    }
+
+    public function setScientificName(string $scientificName): static
+    {
+        $this->scientificName = $scientificName;
+
+        return $this;
+    }
+
+    public function getShortDescription(): ?string
+    {
+        return $this->shortDescription;
+    }
+
+    public function setShortDescription(string $shortDescription): static
+    {
+        $this->shortDescription = $shortDescription;
+
+        return $this;
+    }
+
+    public function getLongDescription(): ?string
+    {
+        return $this->longDescription;
+    }
+
+    public function setLongDescription(?string $longDescription): static
+    {
+        $this->longDescription = $longDescription;
+
+        return $this;
+    }
+
+    public function getPrice(): ?string
+    {
+        return $this->price;
+    }
+
+    public function setPrice(string $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isAvailable(): ?bool
+    {
+        return $this->isAvailable;
+    }
+
+    public function setIsAvailable(bool $isAvailable): static
+    {
+        $this->isAvailable = $isAvailable;
+
+        return $this;
+    }
+
+    public function getAvailableQuantity(): ?int
+    {
+        return $this->availableQuantity;
+    }
+
+    public function setAvailableQuantity(?int $availableQuantity): static
+    {
+        $this->availableQuantity = $availableQuantity;
+
+        return $this;
+    }
+
+    public function getMainImage(): ?string
+    {
+        return $this->mainImage;
+    }
+
+    public function setMainImage(?string $mainImage): static
+    {
+        $this->mainImage = $mainImage;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+}
